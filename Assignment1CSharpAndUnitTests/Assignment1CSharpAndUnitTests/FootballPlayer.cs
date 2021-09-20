f12b76a18db7e72e0e975431765fae6b98efbffe
Patrick Nyskjold <74715124+Patrick09-txt@users.noreply.github.com>
2021-09-20 21:23:11 +0200

    Add files via upload

---
+++ Assignment1CSharpAndUnitTests/Assignment1CSharpAndUnitTests/FootballPlayer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Assignment1CSharpAndUnitTests
+{
+    public class FootballPlayer
+    {
+        private int _id;
+        private string _name;
+        private int _price;
+        private int _shirtNumber;
+
+        public FootballPlayer()
+        {
+
+        }
+
+        public FootballPlayer(int id, string name, int price, int shirtNumber)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            ShirtNumber = shirtNumber;
+        }
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value.Length < 4) throw new ArgumentException("Name must be 4 characters or longer");
+                _name = value;
+            }
+        }
+
+        public int Price
+        {
+            get { return _price; }
+            set
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException("Price", Price, "Price must be above 0");
+                _price = value;
+            }
+        }
+
+        public int ShirtNumber
+        {
+            get { return _shirtNumber; }
+            set
+            {
+                if (value < 1 || value > 100) throw new ArgumentOutOfRangeException("Shirt Number", ShirtNumber, "Shirt Number must be between 1 and 100");
+                _shirtNumber = value;
+            }
+        }
+    }
+}
---
+++ Assignment1CSharpAndUnitTests/Assignment1CSharpAndUnitTestsTests/FootballPlayerTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment1CSharpAndUnitTests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1CSharpAndUnitTests.Tests
+{
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+        private FootballPlayer _footballPlayer;
+
+        [TestInitialize]
+        public void SetupFootballPlayer()
+        {
+            _footballPlayer = new FootballPlayer(1, "Deshawn", 100, 69);
+        }
+
+        [TestMethod]
+        public void TestName()
+        {
+            Assert.AreEqual("Deshawn", _footballPlayer.Name);
+            _footballPlayer.Name = "Jimmy-John";
+            Assert.AreEqual("Jimmy-John", _footballPlayer.Name);
+
+            try
+            {
+                _footballPlayer.Name = "Til";
+                Assert.Fail();
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.AreEqual("Name must be 4 characters or longer", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void TestPrice()
+        {
+            Assert.AreEqual(100, _footballPlayer.Price);
+            _footballPlayer.Price = 250;
+            Assert.AreEqual(250, _footballPlayer.Price);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestPriceException()
+        {
+            _footballPlayer.Price = -250;
+        }
+
+        [TestMethod]
+        public void TestShirtNumber()
+        {
+            Assert.AreEqual(69, _footballPlayer.ShirtNumber);
+            _footballPlayer.ShirtNumber = 42;
+            Assert.AreEqual(42, _footballPlayer.ShirtNumber);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestShirtNumberException()
+        {
+            _footballPlayer.ShirtNumber = 125;
+        }
+    }
+}
